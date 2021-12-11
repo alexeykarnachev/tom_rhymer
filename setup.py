bb142398e848685c3236b54b987803b61d063f12
Alexey Karnachev <alekseykarnachev@gmail.com>
2021-12-11 19:13:44 +0000

    in dev

---
+++ setup.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+import tom_rhymer
+
+_THIS_DIR = pathlib.Path(__file__).parent
+
+
+def _get_requirements():
+    with (_THIS_DIR / 'requirements.txt').open() as fp:
+        return fp.read()
+
+
+setup(
+    name='tom_rhymer',
+    version=tom_rhymer.__version__,
+    install_requires=_get_requirements(),
+    package_dir={'tom_rhymer': 'tom_rhymer'},
+    packages=find_packages(exclude=['tests', 'tests.*']),
+)
